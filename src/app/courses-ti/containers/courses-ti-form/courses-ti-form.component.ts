41b4e91f6f5ae74da66a19e446d3cc74291de0fd
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2023-10-13 21:18:02 -0300

    Add all form array validators and refactor validators and create a form-utils-service class

--- src/app/courses-ti/containers/courses-ti-form/courses-ti-form.component.ts
+++ src/app/courses-ti/containers/courses-ti-form/courses-ti-form.component.ts
@@ -1,105 +1,145 @@
-import { SubjectComputing } from './../../model/subject-computing';
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
-import { CoursesTiService } from '../../services/courses-ti.service';
 import { CoursesTi } from '../../model/courses-ti';
+import { CoursesTiService } from '../../services/courses-ti.service';
+import { SubjectComputing } from './../../model/subject-computing';
+import { FormUtilsService } from 'src/app/shared/form/form-utils.service';
 
 @Component({
   selector: 'app-courses-ti-form',
   templateUrl: './courses-ti-form.component.html',
-  styleUrls: ['./courses-ti-form.component.scss']
+  styleUrls: ['./courses-ti-form.component.scss'],
 })
 export class CoursesTiFormComponent {
-
   form!: FormGroup;
 
   constructor(
+    public formUtils: FormUtilsService,
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesTiService,
     private snackBar: MatSnackBar,
     private location: Location,
     private route: ActivatedRoute
-    ) {
-
-      const course: CoursesTi = this.route.snapshot.data['courseTi'];
-      this.form = this.formBuilder.group({
-        idCourse: [course.idCourse],
-        courseName: [course.courseName, [Validators.required, Validators.minLength(5), Validators.maxLength(250)]],
-        institution: [course.institution, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]],
-        modality: [course.modality, Validators.required],
-        period: [course.period,  Validators.required],
-        city: [course.period,  Validators.required],
-        subjectsComputing: this.formBuilder.array(this.getSubjectsComputing(course))
-      });
+  ) {
+    const course: CoursesTi = this.route.snapshot.data['courseTi'];
+    this.form = this.formBuilder.group({
+      idCourse: [course.idCourse],
+      courseName: [
+        course.courseName,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(250),
+        ],
+      ],
+      institution: [
+        course.institution,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(250),
+        ],
+      ],
+      modality: [course.modality, Validators.required],
+      period: [course.period, Validators.required],
+      city: [course.period, Validators.required],
+      subjectsComputing: this.formBuilder.array(this.getSubjectsComputing(course), Validators.required)
+    });
   }
 
-  private getSubjectsComputing(course: CoursesTi){
+  private getSubjectsComputing(course: CoursesTi) {
     const subjectsComputing = [];
-    if(course?.componentComputingDTOSet){
-      course.componentComputingDTOSet.forEach( subjectComputing => subjectsComputing.push(this.createSubjectComputing(subjectComputing)));
-    }else{
+    if (course?.componentComputingDTOSet) {
+      course.componentComputingDTOSet.forEach((subjectComputing) =>
+        subjectsComputing.push(this.createSubjectComputing(subjectComputing))
+      );
+    } else {
       subjectsComputing.push(this.createSubjectComputing());
     }
     return subjectsComputing;
   }
 
-  private createSubjectComputing(SubjectComputing: SubjectComputing = {idCourse: 0, courseName: '', classHours: 0, syllabus: ''}){
+  private createSubjectComputing(
+    SubjectComputing: SubjectComputing = {
+      idCourse: 0,
+      courseName: '',
+      classHours: 0,
+      syllabus: '',
+    }
+  ) {
     return this.formBuilder.group({
       idCourse: [SubjectComputing.idCourse],
-      courseName: [SubjectComputing.courseName],
-      classHours: [SubjectComputing.classHours],
-      syllabus: [SubjectComputing.syllabus]
-    })
+      courseName: [
+        SubjectComputing.courseName,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(250),
+        ],
+      ],
+      classHours: [
+        SubjectComputing.classHours,
+        [Validators.required, Validators.minLength(1), Validators.maxLength(3)],
+      ],
+      syllabus: [
+        SubjectComputing.syllabus,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(250),
+        ],
+      ],
+    });
   }
 
-  onSubmit(){
-    console.log('teste');
-    this.service.save(this.form.value).subscribe({
-      next: () => {
-        this.onSucess();
-      },
-      error: () => {
-        this.onError();
-   },
-});
+  getSubjectsComputingFormArray() {
+    return (<UntypedFormArray>this.form.get('subjectsComputing')).controls;
   }
 
-  onCancel(){
+  addNewSubjectsComputing() {
+
+    const subjectsComputing = this.form.get('subjectsComputing') as UntypedFormArray;
+    subjectsComputing.push(this.createSubjectComputing());
+
+  }
+
+  removeSubjectsComputing(index: number) {
+    const subjectsComputing = this.form.get(
+      'subjectsComputing'
+    ) as UntypedFormArray;
+    subjectsComputing.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.service.save(this.form.value).subscribe({
+        next: () => {
+          this.onSucess();
+        },
+        error: () => {
+          this.onError();
+        },
+      });
+    }else{
+      this.formUtils.validateAllFormFields(this.form);
+    }
+  }
+
+  onCancel() {
     this.location.back();
   }
 
-  private onError(){
-    this.snackBar.open("Erro ao salvar curso", '', {duration : 5000});
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
   }
 
-  private onSucess(){
-    this.snackBar.open("Curso salvo com sucesso!", '', {duration : 5000});
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
     this.onCancel();
-  }
-
-  getErrorMessage(fieldName: string){
-    const field = this.form.get(fieldName);
-
-    if(field?.hasError('required')){
-      return 'Este campo não pode ser vazio';
-    }
-
-    if(field?.hasError('minlength')){
-      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
-      return `O tamanho mínimo precisa ser de ${requiredLength}`;
-    }
-
-    if(field?.hasError('maxlength')){
-      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 250;
-      return `O tamanho maximo é de ${requiredLength}`;
-    }
-
-    return 'Campo invalido!';
-
   }
 
 }
---
+++ src/app/shared/form/form-utils.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, UntypedFormArray, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormUtilsService {
+
+  constructor() { }
+
+  validateAllFormFields (FormGroup: UntypedFormGroup | UntypedFormArray){
+    Object.keys(FormGroup.controls).forEach( field => {
+      const control = FormGroup.get(field);
+
+      if(control instanceof UntypedFormControl){
+        control.markAsTouched({onlySelf: true});
+      }else if (control instanceof UntypedFormGroup || control instanceof UntypedFormArray){
+        control.markAsTouched({onlySelf: true});
+        this.validateAllFormFields(control);
+      }
+
+    })
+  }
+
+  getErrorMessage(FormGroup: UntypedFormGroup , fieldName: string) {
+    const field = FormGroup.get(fieldName) as UntypedFormControl;
+
+    return this.getErrorMessageFromField(field);
+
+  }
+
+  getErrorMessageFromField (field: UntypedFormControl ) {
+
+
+    if (field?.hasError('required')) {
+      return 'Este campo não pode ser vazio';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors
+        ? field.errors['minlength']['requiredLength']
+        : 5;
+      return `O tamanho mínimo precisa ser de ${requiredLength}`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 250;
+      return `O tamanho maximo é de ${requiredLength}`;
+    }
+  }
+
+  getFormArrayFieldErrorMenssage (FormGroup: UntypedFormGroup, formArrayName: string, fieldName: string, index: number){
+    const formArray = FormGroup.get(formArrayName) as UntypedFormArray;
+    const field = formArray.controls[index].get(fieldName) as UntypedFormControl;
+    return this.getErrorMessageFromField(field);
+
+  }
+
+  isFormArrayValid(FormGroup: UntypedFormGroup, formArrayName : string) {
+    const formArray = FormGroup.get(formArrayName) as UntypedFormArray;
+    return (
+      !formArray.valid &&
+      formArray.hasError('required') &&
+      formArray.touched
+    );
+  }
+
+
+}
